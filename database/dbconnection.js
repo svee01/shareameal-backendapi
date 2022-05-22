0933a1eede6de570a8cd1e5b6e7d02a441ae9d02
sanneve <s.vaneck2@student.avans.nl>
2022-05-22 21:00:51 +0200

    added logger variable

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2')
 require('dotenv').config()
-const { logger } = require('../config/config')
+const { logger } = require('../src/config/config')
 
 const dbConfig = {
     connectionLimit: 100,
