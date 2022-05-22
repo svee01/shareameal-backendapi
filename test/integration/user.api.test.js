9060075716c613894cfafc3269adf0eaf3590537
sanneve <s.vaneck2@student.avans.nl>
2022-05-22 21:02:02 +0200

    added logger variable

--- test/integration/user.api.test.js
+++ test/integration/user.api.test.js
@@ -8,7 +8,6 @@ require('dotenv').config()
 const dbconnection = require('../../database/dbconnection')
 const jwtSecretKey = require('../../src/config/config').jwtSecretKey
 const jwt = require('jsonwebtoken')
-const { logger } = require('../config/config')
 
 chai.should()
 chai.use(chaiHttp)
